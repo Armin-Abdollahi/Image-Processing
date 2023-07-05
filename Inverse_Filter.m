3b862062fa9dec32f49f40b3306eefefd0e53fb0
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-07-05 19:51:48 +0330

    Add files via upload

---
+++ Inverse_Filter.m
@@ -0,0 +1,38 @@
+clc;
+clear all;
+
+%% Read the image and convert to grayscale
+image = imread('Azadi_Square.jpg');
+Grayscale_Image = im2gray(image);
+
+%% Create and apply a Gaussian filter to the original image
+sigma = 3;
+B = imgaussfilt(Grayscale_Image,sigma);
+
+%% Apply a noise to the filtered image
+noisy_image = imnoise(B,'gaussian',0,0.025);
+
+%% Apply the Inverse Filter to the noisy image
+filter = fspecial ('unsharp');
+filtered_image = imfilter(noisy_image,filter);
+
+%% Display the original image and the noisy image and restored image
+subplot(2,3,1);
+imshow(image);
+title('Original Picture');
+
+subplot(2,3,2);
+imshow(Grayscale_Image);
+title('Grayscale Picture');
+
+subplot(2,3,3);
+imshow(B);
+title('Degradation Function');
+
+subplot(2,3,4.5);
+imshow(noisy_image);
+title('Noisy Image');
+
+subplot(2,3,5.5);
+imshow(filtered_image);
+title('Inverse Filter');
