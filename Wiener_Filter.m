1528e60922df5c4e21e4e7f6475ee96139ffb4c3
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-07-05 19:36:47 +0330

    Add files via upload

--- Wiener_Filter.m
+++ Wiener_Filter.m
@@ -2,7 +2,7 @@ clc;
 clear all;
 
 %% Read the image and convert to grayscale
-image = imread('cameraman.jpg');
+image = imread('Azadi_Square.jpg');
 Grayscale_Image = im2gray(image);
 
 %% Create and apply a Gaussian filter to the original image
@@ -16,18 +16,22 @@ noisy_image = imnoise(B,'gaussian',0,0.025);
 K = wiener2(noisy_image, [5,5]);
 
 %% Display the original image and the noisy image and restored image
-subplot(1,4,1);
-imshow(Grayscale_Image);
+subplot(2,3,1);
+imshow(image);
 title('Original Picture');
 
-subplot(1,4,2);
+subplot(2,3,2);
+imshow(Grayscale_Image);
+title('Grayscale Picture');
+
+subplot(2,3,3);
 imshow(B);
 title('Degradation Function');
 
-subplot(1,4,3);
+subplot(2,3,4.5);
 imshow(noisy_image);
 title('Noisy Image');
 
-subplot(1,4,4);
+subplot(2,3,5.5);
 imshow(K);
 title('Wiener Filter');
