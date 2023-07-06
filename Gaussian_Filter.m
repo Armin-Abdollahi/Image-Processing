34654aa14586e3ab59e45ebbc32492793222f566
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-07-06 13:16:45 +0330

    Add files via upload

--- Gaussian_Filter.m
+++ Gaussian_Filter.m
@@ -1,1 +1,23 @@
 clc;
+clear all;
+
+% Read the image
+img = imread('cameraman.jpg');
+
+%% Create gaussian filter
+filter_Size = [5,5];
+sigma = 1;                 % Gaussian filter standard deviation parameter
+guassianFilter = fspecial('gaussian',filter_Size,sigma);
+
+%% Applying the Gaussian filter to the shifted Fourier transform
+filteredImage = imfilter(img,guassianFilter);
+
+%% Display the original image and the reconstructed image
+
+subplot(1,2,1);
+imshow(img);
+title('Original Picture');
+
+subplot(1,2,2);
+imshow(filteredImage);
+title('Filtered Picture');
