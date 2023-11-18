cb0b6875a905eab70fcf0a52f2d87966c9f2d670
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-11-18 15:40:59 +0330

    Add files via upload

--- Adjust_Intensity_Values.py
+++ Adjust_Intensity_Values.py
@@ -8,13 +8,17 @@ from skimage.io import imread
 from skimage import exposure
 import matplotlib.pyplot as plt
 
+
 i = imread('cameraman.jpg')
-j = exposure.rescale_intensity(i, in_range=(0, 1), out_range=(1, 0))
+j = exposure.rescale_intensity(i, in_range=(0, 125), out_range=(0, 1))
 
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-axs[0].imshow(i, cmap='gray')
+axs[0].imshow(i)
 axs[0].set_title('Original')
-axs[1].imshow(j, cmap='gray')
+
+axs[1].imshow(j)
 axs[1].set_title('Adjusted')
+plt.axis('off')
+
 plt.show()
 
