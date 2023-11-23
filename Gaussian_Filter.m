6a67470944466630d022dc880b90f0ac83099c51
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-11-23 22:30:58 +0330

    Add files via upload

--- Gaussian_Filter.m
+++ Gaussian_Filter.m
@@ -9,11 +9,10 @@ filter_Size = [5,5];
 sigma = 1;                 % Gaussian filter standard deviation parameter
 guassianFilter = fspecial('gaussian',filter_Size,sigma);
 
-%% Applying the Gaussian filter to the shifted Fourier transform
+%% Applying the Gaussian filter to the Image
 filteredImage = imfilter(img,guassianFilter);
 
 %% Display the original image and the reconstructed image
-
 subplot(1,2,1);
 imshow(img);
 title('Original Picture');
